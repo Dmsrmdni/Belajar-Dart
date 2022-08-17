8143180048b20f54690cb6c1739fcfd98212caf9
Dmsrmdni <dimasramdani220@gmail.com>
2022-08-17 09:48:51 +0700

    Belajar Dart dasar menambahkan typeData String

---
+++ bin/string.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  String firstName = 'Dimas';
+  String lastName = 'Ramdani';
+
+  print(firstName);
+  print(lastName);
+
+  var fullName = '$firstName $lastName';
+  print(fullName);
+
+  var text = 'aku \'sayang\' \$kamu';
+
+  print(text);
+
+  var text1 =
+      '''aku sayangggg kamuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
+  tapi boongggggggggggg''';
+
+  print(text1);
+}
